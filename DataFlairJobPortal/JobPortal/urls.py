6393eb720d93694add3f8cc37caf571329f0b1b0
Faisal Alrajhi <faisal@salrajhi.com>
2023-03-22 19:54:50 +0000

    this is the first commit

---
+++ DataFlairJobPortal/JobPortal/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.contrib import admin
+#from .views import *
+from .views import home,loginUser,logoutUser,registerUser,applyPage
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('login/',loginUser,name='login'),
+    path('logout/',logoutUser,name='logout'),
+    path('register/',registerUser,name='register'),
+    path('apply/',applyPage,name='apply'),
+]
+
+
+
+
+#from django.urls import path,include
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('',include('JobPortal.urls'))
+#]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
